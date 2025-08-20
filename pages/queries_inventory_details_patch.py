c8c63a0d79e68fd738665d65156888005d18bcc2
Justin Kudela <justinkudela@gmail.com>
2025-08-20 05:54:09 -0400

    Deleted patch files.

--- pages/queries_inventory_details_patch.py
+++
@@ -1,4 +0,0 @@
-# ---- PATCH: Inventory detail by series (with optional Flip IDs and melt) ----
-from typing import List, Optional
-from db import get_conn
- ---- END PATCH ----
--- pages/queries_spending_patch.py
+++
@@ -1,5 +0,0 @@
-# ---- PATCH: Spending Log helpers (BUY spending grouped by date + party) ----
-from typing import List, Optional
-from db import get_conn
-
-# ---- END PATCH ----
