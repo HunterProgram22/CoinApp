07a272bb870d9ebb79453c915f7326adb6a0e92a
Justin Kudela <justinkudela@gmail.com>
2025-09-02 20:49:06 -0400

    Updated Home to fix login.

--- Home.py
+++ Home.py
@@ -3,30 +3,57 @@ import streamlit as st
 import streamlit_authenticator as stauth
 from db import init_db
 from constants import NAVIGATION_ITEMS, APP_TITLE, APP_SUBTITLE
-from auth_utils import get_auth_config
 
 st.set_page_config(page_title=APP_TITLE, page_icon="🪙", layout="wide")
 
-# Initialize authentication
-auth_config = get_auth_config()
-authenticator = stauth.Authenticate(
-    auth_config['credentials'],
-    auth_config['cookie']['name'],
-    auth_config['cookie']['key'],
-    auth_config['cookie']['expiry_days']
-)
 
-# Login widget
-name, authentication_status, username = authenticator.login('Login', 'main')
+# Create authentication config from secrets
+def get_authenticator():
+    """Create authenticator from secrets"""
+    config = {
+        'credentials': {
+            'usernames': {
+                st.secrets.auth.username: {
+                    'email': st.secrets.auth.email,
+                    'failed_login_attempts': 0,
+                    'logged_in': False,
+                    'name': st.secrets.auth.name,
+                    'password': st.secrets.auth.password  # hashed password
+                }
+            }
+        },
+        'cookie': {
+            'expiry_days': st.secrets.cookie.expiry_days,
+            'key': st.secrets.cookie.key,
+            'name': st.secrets.cookie.name
+        }
+    }
 
-if authentication_status == False:
+    # Note: pre-authorized parameter removed in v0.4.2
+    authenticator = stauth.Authenticate(
+        config['credentials'],
+        config['cookie']['name'],
+        config['cookie']['key'],
+        config['cookie']['expiry_days']
+    )
+
+    return authenticator
+
+
+# Initialize authenticator
+authenticator = get_authenticator()
+
+# Create login widget
+authenticator.login()
+
+if st.session_state["authentication_status"] is False:
     st.error('Username/password is incorrect')
-elif authentication_status == None:
+elif st.session_state["authentication_status"] is None:
     st.warning('Please enter your username and password')
-elif authentication_status:
-    # User is authenticated - show the app
-    authenticator.logout('Logout', 'sidebar')
-    st.sidebar.write(f'Welcome *{name}*')
+elif st.session_state["authentication_status"]:
+    # User is authenticated
+    authenticator.logout(location='sidebar')
+    st.sidebar.write(f'Welcome *{st.session_state["name"]}*')
 
     # Initialize database
     init_db()
